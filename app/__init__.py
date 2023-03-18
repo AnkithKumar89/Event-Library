b22e052b6adfe9da6277d1096bbbe4b21f42ddfb
AnkithKumar89 <anaysingh803@gmail.com>
2023-03-18 13:37:48 +0530

    Updated the database address

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb+srv://spenza:spenza@cluster0.csu36sp.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://<username>:<password>@@cluster0.csu36sp.mongodb.net/?retryWrites=true&w=majority")
 db = client.events_db
 custome_event_collection = db.custom_events
 
